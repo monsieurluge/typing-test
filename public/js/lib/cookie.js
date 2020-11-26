4deca1bab44ee8b592ad9f5d6afebd0f5355c9b8
monsieurluge <monsieurluge@gmail.com>
2020-11-26 11:16:37 +0100

    create the cookie lib file

---
+++ public/js/lib/cookie.js
@@ -0,0 +1,12 @@
+const loadCookie = name => fallback => {
+  return document.cookie.split('; ')     // split all the cookies
+    .filter(row => row.startsWith(name)) // keep only the app configs
+    .map(cookie => cookie.split('=')[1]) // take the cookie data
+    .map(JSON.parse)                     // convert them to an object
+    .concat(fallback)                    // add a default content to the list
+    [0]                                  // then return the first content
+}
+
+const saveContentToCookie = name => content => {
+  document.cookie = `${name}=${JSON.stringify(content)}; max-age=31536000; SameSite=Lax`
+}
