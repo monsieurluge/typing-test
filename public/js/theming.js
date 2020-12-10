ba7d5ed40c56c30355deb50475040d334e88c11c
monsieurluge <monsieurluge@gmail.com>
2020-12-10 09:58:59 +0100

    remove unecessary semicolon

--- public/js/theming.js
+++ public/js/theming.js
@@ -8,16 +8,16 @@ let themeColors = {
   errorExtra: "#7e2a33",
   colorfulError: "#ca4754",
   colorfulErrorExtra: "#7e2a33",
-};
+}
 
 function refreshThemeColorObject() {
-  let computedStyle = getComputedStyle(document.body);
+  const computedStyle = getComputedStyle(document.body)
 
-  themeColors.bg = computedStyle.getPropertyValue("--bg-color").replace(" ", "");
-  themeColors.main = computedStyle.getPropertyValue("--accent-color").replace(" ", "");
-  themeColors.caret = computedStyle.getPropertyValue("--accent-color").replace(" ", "");
-  themeColors.sub = computedStyle.getPropertyValue("--sub-color").replace(" ", "");
-  themeColors.text = computedStyle.getPropertyValue("--text-color").replace(" ", "");
-  themeColors.error = computedStyle.getPropertyValue("--error-color").replace(" ", "");
-  themeColors.errorExtra = computedStyle.getPropertyValue("--error-extra-color").replace(" ", "");
+  themeColors.bg = computedStyle.getPropertyValue("--bg-color").replace(" ", "")
+  themeColors.main = computedStyle.getPropertyValue("--accent-color").replace(" ", "")
+  themeColors.caret = computedStyle.getPropertyValue("--accent-color").replace(" ", "")
+  themeColors.sub = computedStyle.getPropertyValue("--sub-color").replace(" ", "")
+  themeColors.text = computedStyle.getPropertyValue("--text-color").replace(" ", "")
+  themeColors.error = computedStyle.getPropertyValue("--error-color").replace(" ", "")
+  themeColors.errorExtra = computedStyle.getPropertyValue("--error-extra-color").replace(" ", "")
 }
